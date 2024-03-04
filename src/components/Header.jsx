da9c4ee84a36cfd14fb3a64ae197aa8e4dbf9dd3
David-Saini <lakshaysaini92558@gmail.com>
2024-03-04 17:00:26 +0530

    header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import HeaderVector from "../assets/Image/webp/headervector.webp"
+import LocationIcon from "../assets/Image/webp/LocationIcon.webp"
+import CelenderIcon from "../assets/Image/png/celendervector.png"
+import arrow from "../assets/Image/png/arrowdown.png"
+import search from "../assets/Image/webp/search.webp"
 
 const Header = () => {
     return (
-        <div className='header_bg !bg-center !bg-cover !bg-no-repeat relative'>
+        <div className='header_bg min_h_screen !bg-center !bg-cover !bg-no-repeat relative'>
             <img className='absolute left-[45%] md:left-[42%] top-[12%] md:top-[8%] max-w-[200px] md:max-w-[270px] lg:max-w-[319px] ' src={HeaderVector} alt="HeaderVector" />
             <div className='max-w-[1440px] mx-auto'>
                 <div className='max-w-[1164px] px-5 md:px-3 mx-auto'>
-                    <div className='pt-[204px] pb-[124px]'>
+                    <div className='lg:pt-[204px] lg:pb-[124px] md:py-[100px] sm:py-20 py-12'>
                         <div className='border border-solid border-white bg-yellow max-w-[400px] sm:max-w-[450px] md:max-w-[470px] lg:max-w-[537px] p-5 sm:p-8 md:p-10 backdrop rounded-[25px] md:rounded-[36px]'>
                             <h1 className='font-bold ff_kanit text-[40px] sm:text-[46px] md:text-[50px] lg:text-[55px] xl:text-xxxl leading-[120%] text-white'>Lets Discover a New Adventure</h1>
                             <p className='text-white ff_kanit leading-[150%] text-base mt-3 sm:mt-4'>Lorem ipsum dolor sit amet consectetur. In ultrices orci urna eget fringilla nisl duis lectus. Tincidunt pretium odio.</p>
@@ -15,13 +19,56 @@ const Header = () => {
                         </div>
                     </div>
 
-                    <div className='bg-white border-white border-solid rounded-[32px] p-8'>
+                    <div className='bg-white border-white border-solid rounded-[32px] p-6 xl:p-8 input_card mt-[-80px] sm:mt-0 translate-y-[20%] sm:translate-y-[40%] lg:translate-y-[50%] xl:translate-y-[70%]'>
 
-                        <div className='gap-8 flex justify-between'>
-                            <div className='flex flex-col gap-2'>
+                        <div className='gap-4 lg:gap-5 xl:gap-8 flex items-center justify-center xl:justify-between flex-wrap'>
+
+                            <div className='flex flex-col gap-1 sm:gap-2'>
                                 <p className='ff_kanit font-light text-base leading-[150%] text-gray1'>From</p>
-                                <input type="text" placeholder='Bali, Indonesia' />
+                                <div className='flex items-center justify-bertween p-3 sm:p-4 border border-solid border-[#c2e2c2] rounded-[48px] sm:max-w-[221px]'>
+                                    <a href="#Location" className='flex items-center'>
+                                        <img className='w-[16px]' src={LocationIcon} alt="" />
+                                        <input type="text" placeholder='Bali, Indonesia' className='ff_kanit font-light text-[14px] leading-[100%] rounded-[48px] outline-none' />
+                                    </a>
+                                    <img className='w-[16px]' src={arrow} alt="arrow" />
+                                </div>
                             </div>
+
+                            <div className='flex flex-col gap-1 sm:gap-2'>
+                                <p className='ff_kanit font-light text-base leading-[150%] text-gray1'>To</p>
+                                <div className='flex items-center justify-bertween p-3 sm:p-4 border border-solid border-[#c2e2c2] rounded-[48px] sm:max-w-[221px]'>
+                                    <a href="#Location" className='flex items-center'>
+                                        <img className='w-[16px]' src={LocationIcon} alt="" />
+                                        <input type="text" placeholder='Bali, Indonesia' className='ff_kanit font-light text-[14px] leading-[100%] rounded-[48px] outline-none' />
+                                    </a>
+                                    <img className='w-[16px]' src={arrow} alt="arrow" />
+                                </div>
+                            </div>
+
+                            <div className='flex flex-col gap-1 sm:gap-2'>
+                                <p className='ff_kanit font-light text-base leading-[150%] text-gray1'>Dates</p>
+                                <div className='flex items-center justify-bertween p-3 sm:p-4 border border-solid border-[#c2e2c2] rounded-[48px] sm:max-w-[221px]'>
+                                    <a href="#Location" className='flex items-center'>
+                                        <img className='w-[16px] pe-2' src={CelenderIcon} alt="CelenderIcon" />
+                                        <input type="text" placeholder='Select a date range' className='ff_kanit font-light text-[14px] leading-[100%] rounded-[48px] outline-none' />
+                                    </a>
+                                    <img className='w-[16px]' src={arrow} alt="arrow" />
+                                </div>
+                            </div>
+
+                            <div className='flex flex-col gap-1 sm:gap-2'>
+                                <p className='ff_kanit font-light text-base leading-[150%] text-gray1'>Guest</p>
+                                <div className='flex items-center justify-bertween p-3 sm:p-4 border border-solid border-[#c2e2c2] rounded-[48px] sm:max-w-[221px]'>
+                                    <a href="#Location" className='flex items-center'>
+                                        <img className='w-[16px]' src={LocationIcon} alt="" />
+                                        <input type="text" placeholder='Number of your guest' className='ff_kanit font-light text-[14px] leading-[100%] rounded-[48px] outline-none' />
+                                    </a>
+                                    <img className='w-[16px]' src={arrow} alt="arrow" />
+                                </div>
+                            </div>
+
+                            <a href="#search"><img className='w-[52px] h-[52px]' src={search} alt="search" /></a>
+
                         </div>
 
                     </div>
