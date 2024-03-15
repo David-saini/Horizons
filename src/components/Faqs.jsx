1752aa7080b8609bbbb2e40d1a4f1c4ba8317ebe
David-Saini <lakshaysaini92558@gmail.com>
2024-03-15 11:19:27 +0530

    faq updates

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -24,20 +24,17 @@ const Faqs = () => {
                         {FaqContent.map((faqitem, index) => (
                             <div key={index} className="pb-4 lg:w-[754px]">
                                 <div className={`bg-white rounded-[10px] ps-5 pe-6 input_card shadow-inputShadow ${expandedIndex === index ? 'active' : ''}`}>
-                                    <h2>
-                                        <button
-                                            type="button"
-                                            className={`flex items-center justify-between w-full text-left text-lg md:text-xl leading-[140%] pt-[16px] !font-light text-dark font-kanit ${expandedIndex ? "pb-[18px]" : "pb-[18px]"} ${expandedIndex === index ? 'text-color' : ''}`}
-                                            onClick={() => toggleAccordion(index)} aria-expanded={expandedIndex === index} aria-controls={`faqs-text-${index}`}>
-                                            <span className={`pe-7 ${expandedIndex === index ? 'text-color1' : ''}`}>{faqitem.title}</span>
+                                    <button
+                                        type="button"
+                                        className={`flex items-center justify-between w-full text-left text-lg md:text-xl leading-[140%] pt-[16px] !font-light text-dark font-kanit ${expandedIndex ? "pb-[18px]" : "pb-[18px]"} ${expandedIndex === index ? 'text-color' : ''}`}
+                                        onClick={() => toggleAccordion(index)} aria-expanded={expandedIndex === index} aria-controls={`faqs-text-${index}`}>
+                                        <span className={`pe-7 ${expandedIndex === index ? 'text-color1' : ''}`}>{faqitem.title}</span>
 
-                                            <svg className={`transform origin-center transition duration-200 ease-out ${expandedIndex === index ? 'rotate-180 svg-color' : 'svg-color'}`} width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M1 1L7.5 8L14 1" stroke="#000702" strokeWidth="2" />
-                                            </svg>
-                                        </button>
-                                    </h2>
-                                    <div id={`faqs-text-${index}`} role="region" aria-labelledby={`faqs-title-${index}`} className={`grid text-sm text-slate-600 overflow-hidden transition-all duration-300 ease-in-out ${expandedIndex === index ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}
-                                    >
+                                        <svg className={`transform origin-center transition duration-200 ease-out ${expandedIndex === index ? 'rotate-180 svg-color' : 'svg-color'}`} width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M1 1L7.5 8L14 1" stroke="#000702" strokeWidth="2" />
+                                        </svg>
+                                    </button>
+                                    <div id={`faqs-text-${index}`} role="region" aria-labelledby={`faqs-title-${index}`} className={`grid text-sm text-slate-600 overflow-hidden transition-all duration-300 ease-in-out ${expandedIndex === index ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}>
                                         <div className="overflow-hidden">
                                             <p className={`pb-4 text-dark opacity-70 font-kanit font-light text-base max-w-[679px] ${expandedIndex === index ? 'text-color' : ''}`}>{faqitem.content}</p>
                                         </div>
